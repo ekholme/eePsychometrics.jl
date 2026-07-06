e6f56c0e082038fe48a156f46639c25dd785a7cf
Documenter.jl <documenter@juliadocs.github.io>
2026-07-06 20:08:52 +0000

    build based on 0a9c306

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"category":"section","location":"#eePsychometrics","page":"Home","text":"Documentation for eePsychometrics.\n\n","title":"eePsychometrics"}]
+}
